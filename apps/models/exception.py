844a98a9fb8eec01606e9a9f9ccb9438fe52431d
alexiscaspell <alexiscaspell@gmail.com>
2021-09-06 20:14:54 -0300

    Retoques excepciones de db

--- apps/models/exception.py
+++ apps/models/exception.py
@@ -50,3 +50,9 @@ class UsuarioNotFoundException(AppException):
 
     def __str__(self):
         return self.mensaje
+class DatabaseException(AppException):
+    def __init__(self,mensaje="Error en base de datos."):
+        super().__init__(500, mensaje)
+
+    def __str__(self):
+        return self.mensaje
