d8670c26be98b4255d4428ed804e8e1c40393aa9
alexiscaspell <alexiscaspell@gmail.com>
2021-09-13 17:32:30 -0300

    Fix insert solo la primera vez en sensor

--- apps/repositories/sensor_repository.py
+++ apps/repositories/sensor_repository.py
@@ -8,7 +8,8 @@ from apps.utils.mongo.mongo_connector import MongoQueryBuilder
 
 def _get_sensores():
     try:
-        query = MongoQueryBuilder(SensorDocument).add_exclude_field("_id").build()
+        query = MongoQueryBuilder(
+            SensorDocument).add_exclude_field("_id").build()
         resultados = mongo_connector.get_by_filter(query)
 
         if not resultados:
@@ -20,12 +21,29 @@ def _get_sensores():
 
     return []
 
+
 def get_all() -> List[Sensor]:
     return _get_sensores()
 
-def save_all(sensores:List[Sensor]):
-    for s in sensores:
-        SensorDocument.easy_update_one_by_key("MAC",s.MAC,SensorDocument.from_model(s).to_dict(),args={"upsert":True})
 
-def save(sensor:Sensor):
+def save_all(sensores: List[Sensor]):
+    macs = list(set(map(lambda s:s.MAC,sensores)))
+
+    query = MongoQueryBuilder(SensorDocument).add_exclude_field("_id")
+    query.add_exclude_field("id")
+    query.add_filter({"MAC":{"$in":macs}})
+
+    resultados = mongo_connector.get_by_filter(query.build())
+
+    macs_encontradas = list(map(lambda r:r["MAC"],resultados))
+
+    for s in sensores:
+        if s.MAC in macs_encontradas:
+            continue
+
+        save(s)
+    # SensorDocument.easy_update_one_by_key("MAC",s.MAC,SensorDocument.from_model(s).to_dict(),args={"upsert":True})
+
+
+def save(sensor: Sensor):
     mongo_connector.insert(SensorDocument.from_model(sensor))
