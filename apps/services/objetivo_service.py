761d0a96ce4114aba4ffdcf35b16cc5f05d745a5
alexiscaspell <alexiscaspell@gmail.com>
2021-09-16 23:46:37 -0300

    Update objetivo_service.py

--- apps/services/objetivo_service.py
+++ apps/services/objetivo_service.py
@@ -1,46 +1,39 @@
 from datetime import datetime
-from typing import Dict,List
-from apps.models.app_model import AppModel,model_metadata
-from apps.models.indicador import Indicador
-from enum import Enum
-from apps.models.exception import ObjetivoInvalidoException
+from typing import Dict, List
+from apps.models.objetivo import Objetivo,ObjetivoResult, ObjetivoStatus
+from apps.repositories import tablero_repository
+from apps.services.indicador_service import get_mediciones,get_indicador
 
-class ObjetivoStatus(Enum):
-    pendiente = "pendiente"
-    no_cumplido = "no_cumplido"
-    cumplido = "cumplido"
-@model_metadata({"status":ObjetivoStatus})
-class ObjetivoResult(AppModel):
-    def __init__(self,result_spec):
-        self.id_objetivo = result_spec["id_objetivo"]
-        self.status = result_spec["status"]
-        self.valor = result_spec.get("valor",None)
-        self.valor_esperado = result_spec.get("valor_esperado",None)
+def get_objetivo(id_tablero,id_objetivo)->Objetivo:
+    return tablero_repository.get_objetivo(id_tablero,id_objetivo)
 
-@model_metadata({"fecha_inicial":datetime,"fecha_final":datetime,"valor":float})
-class Objetivo(AppModel):
-    def __init__(self, objetivo_spec: Dict):
-        self.id = objetivo_spec.get("id", None)
-        self.nombre = objetivo_spec["nombre"]
-        self.descripcion = objetivo_spec.get("descripcion","")
-        self.fecha_inicial = objetivo_spec.get("fecha_inicial",None)
-        self.fecha_final = objetivo_spec.get("fecha_final",None)
+def procesar_objetivo(id_tablero:str,id_objetivo:str) -> ObjetivoResult:
+    objetivo:Objetivo = get_objetivo(id_tablero, id_objetivo)
+    indicador = get_indicador(id_tablero,objetivo.id_indicador)
 
-        self.id_indicador = objetivo_spec["id_indicador"]
-        self.nombre_indicador = objetivo_spec["nombre_indicador"]
+    counts = []
 
-        self.valor = objetivo_spec.get("valor",None)
-        self.funcion = None
+    for sensor in indicador.sensores:
+        mediciones = get_mediciones(sensor,desde=objetivo.fecha_inicial, hasta=objetivo.fecha_final)
 
-    def evaluar(self,valor:float)->ObjetivoStatus:
-        if self.valor is None:
-            raise ObjetivoInvalidoException(self.id)
-        
-        fecha_actual=datetime.now()
+        count=0
 
-        status = ObjetivoStatus.cumplido if valor>=self.valor and fecha_actual>self.fecha_final else ObjetivoStatus.pendiente
+        for m in mediciones:
+            if m.valor<= indicador.limite_superior:
+                if indicador.limite_inferior is None or m.valor>=indicador.limite_inferior:
+                    count+=1 
 
-        if datetime.now()> self.fecha_final and status==ObjetivoStatus.pendiente:
-            status = ObjetivoStatus.no_cumplido
+        counts.append({"sensor":sensor.id,"contador":count,"mediciones":len(mediciones)})
 
-        return status
+    mediciones_totales=0
+    contador_total=0
+
+    for c in counts:
+        contador_total+=c["contador"]
+        mediciones_totales+=c["mediciones"]
+
+    valor = contador_total/max(mediciones_totales,1)*100
+
+    status = objetivo.evaluar(valor)
+
+    return ObjetivoResult.from_dict({"id_objetivo":id_objetivo,"status":status,"valor":valor,"valor_esperado":objetivo.valor})
