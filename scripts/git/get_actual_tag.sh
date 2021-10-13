b74d6c2871720b2fc3aaaeb3535b21ff3ed8588e
ataberna-rb <alexis.taberna@redb.ee>
2021-10-13 02:57:07 -0300

    Fix get actual tag

--- scripts/git/get_actual_tag.sh
+++ scripts/git/get_actual_tag.sh
@@ -1,6 +1,7 @@
-LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-LATEST_TAG="${LATEST_TAG/'refs/heads/'/''}"
-LATEST_TAG="${LATEST_TAG/'v'/''}"
+# LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+# LATEST_TAG="${LATEST_TAG/'refs/heads/'/''}"
+# LATEST_TAG="${LATEST_TAG/'v'/''}"
+LATEST_TAG=$(git tag |sort -rV | head -1)
 
 echo $LATEST_TAG
 
