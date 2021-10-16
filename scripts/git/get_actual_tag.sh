87ec3316502bf049fd255998490c0885e939a926
alexiscaspell <alexiscaspell@gmail.com>
2021-10-16 01:11:45 -0300

    Update get_actual_tag.sh

--- scripts/git/get_actual_tag.sh
+++ scripts/git/get_actual_tag.sh
@@ -1,7 +1,6 @@
-# LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
-# LATEST_TAG="${LATEST_TAG/'refs/heads/'/''}"
-# LATEST_TAG="${LATEST_TAG/'v'/''}"
 LATEST_TAG=$(git tag |sort -rV | head -1)
+LATEST_TAG="${LATEST_TAG/'refs/heads/'/''}"
+LATEST_TAG="${LATEST_TAG/'v'/''}"
 
 echo $LATEST_TAG
 
